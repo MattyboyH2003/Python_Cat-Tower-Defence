b87371b5b0e34a85693c9a5a8055e42064a21967
Mattyboy <Matthewhunter773@btinternet.com>
2020-07-14 11:19:28 +0100

    Update Files and Add GUI Image

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame import *
-#from Towers import *
+from Towers import *
 
 #Setup
 if __name__ == "__main__":
@@ -8,14 +8,21 @@ if __name__ == "__main__":
     resolution = (1280, 720)
     pygame.display.set_caption("Cat Shooty Game")
     window = pygame.display.set_mode(resolution)
+    #pygame.display.set_icon(surface)
     background = pygame.image.load("Sprites\Towers\AngryCatSprite.png")
     clock = pygame.time.Clock()
     running = True
 
-#towers = Towers()
+    currentTower = PistolCat
+    TowersList = []
+else:
+    exit()
 
+#towers = Towers()
 def PlaceTower():
-    pass
+    mousePositon = pygame.mouse.get_pos()
+
+    window.blit(pygame.image.load(currentTower.GetSprite()), mousePositon)
     """
     To place a tower you will need:
     A sprite (defined in the tower's class data)
@@ -29,7 +36,19 @@ while running == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+        
+        if event.type == pygame.MOUSEBUTTONUP:
+            PlaceTower()
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                currentTower = PistolCat
+            if event.key == pygame.K_RIGHT:
+                currentTower = AngryCat
+
+
+
+
     window.blit(background, [0, 0])
     pygame.display.update()
     clock.tick(30)
--- Towers.py
+++ Towers.py
@@ -1,21 +1,43 @@
+from pygame import math
+
+
 class Towers:
 
-        """
-        Classes of towers:
-        Pistol Cat
-        Rifler Cat
-        Ranger Cat
-        Angry Cat
-        """
+    """
+    Classes of towers:
+    Pistol Cat
+    Rifler Cat
+    Ranger Cat
+    Angry Cat
+    """
 
-    def __init__(self, screen):
-        pass
+    def __init__(self):
+        self.test = 1
 
 #Towers 
-class Pistol(Towers):
+class PistolCat(Towers):
 
     sprite = "Sprites\Towers\PistolCatSprite.png"
 
     def __init__(self):
         self.range = 10 # 1 range unit = 10 pixel radius
         self.damage = 1 # Damage is equal to units unravelled per attack
+        self.location = math.Vector2()
+    
+    @classmethod
+    def GetSprite(cls):
+        return cls.sprite
+
+#Towers 
+class AngryCat(Towers):
+
+    sprite = "Sprites\Towers\AngryCatSprite.png"
+
+    def __init__(self):
+        self.range = 2 # 1 range unit = 10 pixel radius
+        self.damage = 1 # Damage is equal to units unravelled per attack
+        self.location = math.Vector2()
+    
+    @classmethod
+    def GetSprite(cls):
+        return cls.sprite
