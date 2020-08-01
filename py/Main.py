2132ef6fc874036e15e6e979d0ce4afad8378228
WillowRo <nyeevans@outlook.com>
2020-08-01 01:52:33 +0100

    pushed more maps

--- py/Main.py
+++ py/Main.py
@@ -39,7 +39,7 @@ class Main():
     upgrading = False
     currentTower = 0
     selectedTower = None
-    currentMap = "Data\Maps\map5.txt"
+    currentMap = "Data\\Maps\\map7.txt"
     
 
     buttonList = []
